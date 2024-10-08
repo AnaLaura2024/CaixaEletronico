8a42754f25822d1efb512a8bd9d2c98280729ffc
AnaLaura2024 <0001065196@senaimgaluno.com.br>
2024-10-08 14:22:10 -0300

    CaixaEletronico

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Definição de variáveis
+        int contaNumero = 123456;
+        double saldo = 0;
+        int opcao;
+        double valor;
+
