6e407b3d7b6c8c9d4addc802720ee1216ce69816
AnaLaura2024 <0001065196@senaimgaluno.com.br>
2024-10-08 14:22:57 -0300

    CaixaEletronico

--- Program.cs
+++ Program.cs
@@ -7,4 +7,15 @@
         double saldo = 0;
         int opcao;
         double valor;
+                while (true)
+        {
+            // Menu de opções
+            Console.WriteLine("\n--- Caixa Eletrônico ---");
+            Console.WriteLine("1. Depósito");
+            Console.WriteLine("2. Saque");
+            Console.WriteLine("3. Extrato");
+            Console.WriteLine("4. Sair");
+            Console.Write("Escolha uma opção: ");
+            opcao = Convert.ToInt32(Console.ReadLine());
+
 
